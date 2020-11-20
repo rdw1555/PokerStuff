81376fe4e7ca23adbd8e9f4db827c7831747e093
Michael Holtz <mjh9122@rit.edu>
2020-11-20 11:45:15 -0500

    Rit viewer complete with comments

--- src/gui/RITViewer.java
+++ src/gui/RITViewer.java
@@ -14,15 +14,20 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class RITViewer extends Application {
+    //private field fileName, representing the uncompressed file
     private static String fileName;
 
     @Override
     public void start(Stage stage) throws Exception {
+        //Creates group to but the canvas in
         Group root = new Group();
+        //Creates Canvas
         Canvas canvas = new Canvas(512, 512);
         GraphicsContext gc = canvas.getGraphicsContext2D();
         root.getChildren().add(canvas);
+        //ArrayList to read in pixel values from file
         ArrayList<Integer>image = new ArrayList<>();
+        //Read in the pixel values
         Scanner in = null;
         try {
             File imageFile = new File(fileName);
@@ -33,6 +38,7 @@ public class RITViewer extends Application {
         while(in.hasNextInt()){
             image.add(in.nextInt());
         }
+        //Print pixels from arrayList to the canvas
         int imageSize = (int)Math.sqrt(image.size());
         int squareSize = 512/imageSize;
         for(int i = 0; i < imageSize; i++){
@@ -42,6 +48,7 @@ public class RITViewer extends Application {
                 gc.fillRect(j*squareSize,i*squareSize,squareSize,squareSize);
             }
         }
+        //Add scene to stage and show stage
         stage.setScene(new Scene(root));
         stage.show();
 
